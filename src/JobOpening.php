6dbc0813e8adbbd3382e48e11fed24ea93b53975
Joseph Karasek and matt knutson <mknut001@gmail.com>
2016-02-09 10:34:04 -0800

    add object JobPosting with getters

---
+++ src/JobOpening.php
@@ -0,0 +1,28 @@
+<?php
+    class JobOpening
+    {
+        private $title = '';
+        private $description = '';
+        private $contact_info = '';
+
+        function __construct($title, $description, $contact_info)
+        {
+            $this->title = $title;
+            $this->description = $description;
+            $this->contact_info = $contact_info;
+        }
+        function getTitle()
+        {
+            return $this.title;
+        }
+        function getDescription()
+        {
+            return $this.description;
+        }
+        function getContactInfo()
+        {
+            return $this.contact_info;
+        }
+
+    }
+ ?>
